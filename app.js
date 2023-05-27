68d5e779bbb3d3725f154717a3246dff6f6d631a
Nikola Murgovski <62479996+nikolamurgo@users.noreply.github.com>
2023-05-27 23:08:20 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var loginModal = document.getElementById('loginModal');
-    var loginButton = document.getElementById('login');
-    var closeButton = document.getElementsByClassName('close')[0];
+// document.addEventListener('DOMContentLoaded', function() {
+//     var loginModal = document.getElementById('loginModal');
+//     var loginButton = document.getElementById('login');
+//     var closeButton = document.getElementsByClassName('close')[0];
 
-    loginButton.addEventListener('click', function() {
-        loginModal.style.display = 'block';
-    });
+//     loginButton.addEventListener('click', function() {
+//         loginModal.style.display = 'block';
+//     });
 
-    closeButton.addEventListener('click', function() {
-        loginModal.style.display = 'none';
-    });
-});
+//     closeButton.addEventListener('click', function() {
+//         loginModal.style.display = 'none';
+//     });
+// });
 
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    var registerModal = document.getElementById('registerModal');
-    var loginButton = document.getElementById('register');
-    var closeButton = document.getElementsByClassName('close')[0];
+// document.addEventListener('DOMContentLoaded', function() {
+//     var registerModal = document.getElementById('registerModal');
+//     var loginButton = document.getElementById('register');
+//     var closeButton = document.getElementsByClassName('close')[0];
 
-    loginButton.addEventListener('click', function() {
-        registerModal.style.display = 'block';
-    });
+//     loginButton.addEventListener('click', function() {
+//         registerModal.style.display = 'block';
+//     });
 
-    closeButton.addEventListener('click', function() {
-        registerModal.style.display = 'none';
-    });
-});
+//     closeButton.addEventListener('click', function() {
+//         registerModal.style.display = 'none';
+//     });
+// });
 
-document.addEventListener('DOMContentLoaded', function() {
-    var nextthing = document.getElementById('nextthing');
-    var loginButton = document.getElementById('register');
-    var closeButton = document.getElementsByClassName('close')[0];
+// document.addEventListener('DOMContentLoaded', function() {
+//     var nextthing = document.getElementById('nextthing');
+//     var loginButton = document.getElementById('register');
+//     var closeButton = document.getElementsByClassName('close')[0];
 
-    loginButton.addEventListener('click', function() {
-        nextthing.style.display = 'block';
-    });
+//     loginButton.addEventListener('click', function() {
+//         nextthing.style.display = 'block';
+//     });
 
-    closeButton.addEventListener('click', function() {
-        nextthing.style.display = 'none';
-    });
-});
+//     closeButton.addEventListener('click', function() {
+//         nextthing.style.display = 'none';
+//     });
+// });
